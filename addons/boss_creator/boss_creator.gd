a46698948d5b699f96d06cdff905deee3b614164
Gusti Rama Henry <g.ramahenry2439@gmail.com>
2024-04-14 22:17:38 -0400

    init

---
+++ addons/boss_creator/boss_creator.gd
@@ -0,0 +1,15 @@
+@tool
+extends EditorPlugin
+
+
+var test := "string"
+
+func _enter_tree() -> void:
+	# Initialization of the plugin goes here.
+	pass
+
+
+func _exit_tree() -> void:
+	# Clean-up of the plugin goes here.
+	pass
+
---
+++ addons/boss_creator/inspector.gd
@@ -0,0 +1,10 @@
+@tool
+extends EditorInspectorPlugin
+
+
+func _can_handle(object: Object) -> bool:
+	if object.get_class() == "Boss":
+		return true
+
+	else:
+		return false
